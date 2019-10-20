8e413daf4884eb817cad64278d110531c773a856
ClaraRus <43996861+ClaraRus@users.noreply.github.com>
2019-10-20 16:24:28 +0300

    -first commit

---
+++ TravelChatApp/obj/Debug/90/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 5d0c814eae05274e2e41c2df76b895ffbbe7ecf6 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1147
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 300515
+	.include	"typemap.mj.inc"
